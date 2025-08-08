76ccc424a61f1ed61d1fe7e83c9fb92963694070
marjan-1064 <marjanfatima31@gmail.com>
2025-08-08 11:46:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// var x="hello world!";
+// var message="in global";
+// console.log("golbal:message= " + message);
+// var a=function (){
+// var message="inside a";
+// console.log("a:message = " + message);
+// b();
+// }
+// a();
+// function b(){
+//     console.log("b:message= " + message);
+// }
+var names=["john","ali" ,"jane" ,"sara"];
+for(var i=0;i<4;i++){
+    var character=names[i];
+
+if(character[0]=="j"){
+    console.log("goodbye"+character);
+}
+else{
+    console.log("hello"+character);
+}
+    }
+
+
